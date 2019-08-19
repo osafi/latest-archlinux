d2cfaa8ee258b3867888125986720a10306ba938
Omeed Safi <omeed@safi.ms>
2019-08-18 20:20:26 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,4 +4,4 @@ import makeApp from './src/Service';
 const port = process.env.PORT || 3000;
 
 const app = makeApp(downloadScraper);
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`listening on port ${port}`))
