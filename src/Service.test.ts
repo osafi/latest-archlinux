998c19c6706a6edd72a86fdb84cd325833cc18a4
Omeed Safi <omeed@safi.ms>
2019-08-19 23:49:02 -0700

    Return error from service when unable to scrape download URLs

--- src/Service.test.ts
+++ src/Service.test.ts
@@ -4,36 +4,44 @@ import { DownloadScraperFunction } from './DownloadScraper';
 import makeApp from './Service';
 
 describe('Service', () => {
-  it('redirects to worldwide region ISO from scraper', async () => {
-    const scraper = td.function<DownloadScraperFunction>();
+  let scraper: DownloadScraperFunction;
+  let subject: Express.Application;
 
+  beforeEach(() => {
+    scraper = td.function<DownloadScraperFunction>();
+    subject = makeApp(scraper);
+  });
+
+  it('redirects to worldwide region ISO from scraper', async () => {
     td.when(scraper()).thenResolve([
       { location: 'United States', urls: ['http://usa.com/arch.iso'] },
       { location: 'Worldwide', urls: ['http://worldwide.com/arch.iso'] },
     ]);
 
-    const app = makeApp(scraper);
-
-    await request(app)
+    await request(subject)
       .get('/iso')
       .expect(307)
       .expect('Location', 'http://worldwide.com/arch.iso');
   });
 
   it('redirects to URL for region based on query param', async () => {
-    const scraper = td.function<DownloadScraperFunction>();
-
     td.when(scraper()).thenResolve([
       { location: 'Worldwide', urls: ['http://worldwide.com/arch.iso'] },
       { location: 'United States', urls: ['http://usa.com/arch.iso'] },
     ]);
 
-    const app = makeApp(scraper);
-
-    await request(app)
+    await request(subject)
       .get('/iso')
       .query({ region: 'united_states' })
       .expect(307)
       .expect('Location', 'http://usa.com/arch.iso');
+  });
+
+  it('returns 500 when scraper fails', async () => {
+    td.when(scraper()).thenReject(new Error('scraper failed'));
+
+    await request(subject)
+      .get('/iso')
+      .expect(500, { message: 'Unable to fetch download URLs' });
   });
 });
--- src/Service.ts
+++ src/Service.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 import { DownloadScraperFunction } from './DownloadScraper';
 
-export default function(scraper: DownloadScraperFunction): express.Express {
+export default function(scraper: DownloadScraperFunction): Express.Application {
   const app = express();
 
   app.get('/iso', async (req, res, _next) => {
@@ -10,12 +10,16 @@ export default function(scraper: DownloadScraperFunction): express.Express {
       ? req.query.region.replace('_', ' ')
       : 'Worldwide';
 
-    const downloadRegions = await scraper();
-    const region = downloadRegions.find(
-      r => r.location.toLowerCase() === requestedRegion.toLowerCase(),
-    );
+    try {
+      const downloadRegions = await scraper();
+      const region = downloadRegions.find(
+        r => r.location.toLowerCase() === requestedRegion.toLowerCase(),
+      );
 
-    res.redirect(307, region!.urls[0]);
+      res.redirect(307, region!.urls[0]);
+    } catch (err) {
+      res.status(500).send({ message: 'Unable to fetch download URLs' });
+    }
   });
 
   return app;
